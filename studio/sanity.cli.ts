83a7cb6cb59f519bf6d93708001781092797a226
Tradetitan66 <tradetitan66@gmail.com>
2026-09-03 06:44:27 +0100

    chore: configure sanity studio hostname and deployment app id

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -8,4 +8,8 @@ export default defineCliConfig({
     projectId,
     dataset,
   },
+  studioHost: process.env.SANITY_STUDIO_HOST || 'execora',
+  deployment: {
+    appId: 'g5kbxnt5cfg1xf3d2c8ac2pc',
+  },
 })
